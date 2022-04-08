10926086c0d0bb2011887fe66d728ed131966da5
Aurisfoxtro <94296988+Aurisfoxtro@users.noreply.github.com>
2022-04-08 12:05:39 +0300

    React namu darbas

--- Paskaitos/REACT/22-04-07-todo/src/components/task/Task.jsx
+++ Paskaitos/REACT/22-04-07-todo/src/components/task/Task.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import { ListGroup, Button } from "react-bootstrap";
 import { useGlobalContext } from "../../context/TasksContext";
 
 const Task = (props)=>{
     const {removeTask} =useGlobalContext();
+    const [showButton, setShowButton] = useState(true);
+    const [line, setLine] = useState({
+        'textDecoration': ''
+    })
+
+    const lineHandler =()=>{
+        setLine({'textDecoration': 'line-through'});
+        setShowButton(false);
+    }
+
     return(
-        <ListGroup.Item>{props.title}: {props.desc}<Button onClick={()=>{removeTask(props.id)}} className="float-end">Šalinti</Button></ListGroup.Item>
+        <ListGroup.Item><div style={line}>{props.title}: {props.desc}</div>
+        <Button onClick={()=>{removeTask(props.id)}} className="float-end">Šalinti</Button>
+        {showButton && <Button onClick={lineHandler} className = "float-end me-2">Atlikta</Button>}
+        </ListGroup.Item>
     )
 }
 
