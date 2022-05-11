31b53292f49cc9b75a29280b5d69e46bff4f9384
Aurisfoxtro <94296988+Aurisfoxtro@users.noreply.github.com>
2022-05-11 09:14:15 +0300

    Nuotrauku galerija Express  Handlebars Database insert select

---
+++ Paskaitos/NODE/22-05-09/helpers/db.js
@@ -0,0 +1,12 @@
+import database from "./config.js"
+// import mysql from "mysql2"
+//Myslq modulio su promisais importavimas
+import mysql from "mysql2/promise"
+
+const connection = await mysql.createConnection({
+    host: database.database_host,
+    user: database.user,
+    database: database.database
+})
+
+export default connection
--- Paskaitos/NODE/22-05-09/uploader.js
+++ Paskaitos/NODE/22-05-09/uploader.js
@@ -1,10 +1,12 @@
 import database from "./helpers/config.js"
 import {port} from "./helpers/config.js"
-//Myslq modulio su promisais importavimas
-import mysql from "mysql2/promise"
+import db from "./helpers/db.js"
+//Myslq2 modulio su promisais importavimas
+// import mysql from "mysql2/promise"
 import express from 'express'
 import {create} from 'express-handlebars'
 import multer from 'multer'
+import {rm} from 'fs/promises'
 
 const app = express()
 const handlebars = create()
@@ -13,31 +15,84 @@ const handlebars = create()
 const diskStorage = multer.diskStorage({
     destination: (req, file, callback)=>{
         callback(null, './uploads')
+    },
+    filename: (req, file, callback)=>{
+        const ext = file.originalname.split('.')
+
+        // console.log(Date.now())
+        // console.log(Date.now() + '.' + ext[1])
+        // console.log(req)
+        // console.log(file)
+        callback(null, Date.now() + '.' + ext[1])
     }
 })
 
 const upload = multer({
-    storage: diskStorage
+    storage: diskStorage,
+    fileFilter: (req, file, callback)=>{
+        //Atliekamas failu formato tikrinimas
+        if(
+            file.mimetype === 'image/jpeg' ||
+            file.mimetype === 'image/png' ||
+            file.mimetype === 'image/gif'
+        ){
+            callback(null, true)
+        }else{
+            callback(null, false)
+        }
+        // console.log(file)
+    }
 })
 
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
-const connection = await mysql.createConnection({
-    host: database.database_host,
-    user: database.user,
-    database: database.database
-})
+//nurodom, kad naudosim assetus, siam pvz nuotraukas
+app.use('/uploads', express.static('uploads'))
 
-const table = 'pictures'
+app.use('/assets/css', express.static('assets/css'))
 
-app.get('/', (req, res)=>{
-    res.render('form')
+// const connection = await mysql.createConnection({
+//     host: database.database_host,
+//     user: database.user,
+//     database: database.database
+// })
+
+// const table = 'pictures'
+
+app.get('/', async (req, res)=>{
+    const photos = await db.query('SELECT * FROM pictures')
+    console.log(photos)
+    res.render('form', {photos: photos[0]})
 })
-app.post('/upload_file', upload.single('photo'), (req, res)=>{
-    console.log(req.file)
-    res.send('Failas gautas')
+app.post('/upload_file', upload.single('photo'), async (req, res)=>{
+    if(req.file === undefined){
+        res.send('Netinkamas failas')
+    } else{
+        let insert
+        console.log(req.body)
+        if(req.body.author !== ''){
+            insert = await db.query(`INSERT INTO pictures (picture, author) VALUES ('${req.file.filename}','${req.body.author}')`)    
+        }
+        if(req.body.description !== ''){
+            insert = await db.query(`INSERT INTO pictures (picture, description) VALUES ('${req.file.filename}','${req.body.description}')`)    
+        }
+        if(req.body.description !== '' && req.body.author !== ''){
+            insert = await db.query(`INSERT INTO pictures (picture, author, description) VALUES ('${req.file.filename}','${req.body.author}','${req.body.description}')`)
+        }
+        if(req.body.author === '' && req.body.description === ''){
+            // console.log(req.file)
+            insert = await db.query(`INSERT INTO pictures (picture) VALUES ('${req.file.filename}')`)
+            // const insert = await db.query('INSERT INTO pictures (picture) VALUES (?)', req.file.filename)
+        }
+        if(insert[0].affectedRows == 1){
+            res.send('Failas gautas <a href="/">Į titulinį</a>')
+        }else{
+            await rm('./uploads/' + req.file.filename)
+            res.send('Nepavyko įkelti failo i duomenų bazę')
+        }
+    }
 })
 
 app.listen(port)
