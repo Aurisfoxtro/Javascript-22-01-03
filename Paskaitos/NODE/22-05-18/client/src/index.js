69719df49af9e50127e9df4c6561d06ceff1569b
Aurisfoxtro <94296988+Aurisfoxtro@users.noreply.github.com>
2022-05-23 11:54:11 +0300

    Freelancer App update

--- Paskaitos/NODE/22-05-18/client/src/index.js
+++ Paskaitos/NODE/22-05-18/client/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Registration from './components/registration/Registration.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/registration" element={<Registration/>}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
